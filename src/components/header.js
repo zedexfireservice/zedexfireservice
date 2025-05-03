413f25be8526da26897975e1e98a8efb0ab17df8
decodive <amanalpha2492@gmail.com>
2025-05-03 16:54:25 +0530

    blog added

--- src/components/header.js
+++ src/components/header.js
@@ -58,7 +58,7 @@ function Header() {
 
 
     { text: 'Our Clients', href: '/our-clients' },
-    { href: '/blogs', text: 'Blogs' },
+    { href: '/top-Fire-Fighting-Company-in-Delhi-NCR', text: 'Blogs' },
 
     { text: 'CONTACT US', href: '/contact-Us' },
   ];
@@ -143,7 +143,7 @@ function Header() {
     },
     {
       label: "Blogs",
-      href:"/blogs"
+      href:"/top-Fire-Fighting-Company-in-Delhi-NCR"
     },
 
     {
