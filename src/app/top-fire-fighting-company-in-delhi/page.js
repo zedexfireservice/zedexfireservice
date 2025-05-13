da3be91c746b3077af9ed555794b106b458dbe3b
decodive <amanalpha2492@gmail.com>
2025-05-13 16:30:26 +0530

    zedex 12

---
+++ src/app/top-fire-fighting-company-in-delhi/page.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import TopCompany from './TopCompany'
+
+
+export const metadata = {
+
+    title: "Top Fire Fighting Company in Delhi NCR | Zedex Fire Services",
+    description: "Looking for the best firefighting company in Delhi NCR? Zedex Fire Services is ISO 9001:2008 certified, offering top-quality systems & equipment. Contact us today!",
+  
+  }
+
+export default function page() {
+  return (
+    <>
+    
+     <TopCompany/>
+     
+     </>
+  )
+}
