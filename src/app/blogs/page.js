f417b0439bd09842cb492dee7d265b2f3b15589d
decodive <amanalpha2492@gmail.com>
2025-05-13 16:03:49 +0530

    zedex 9

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -7,7 +7,7 @@ export default function page() {
 const blogsCards  = [
 
   {
-    href:'top-fire-fighting-company-in-delhi-ncr',
+    href:'/top-fire-fighting-company-in-delhi-ncr',
     img:"https://res.cloudinary.com/dgx5cntyb/image/upload/v1746618767/topfire_jx2sgb.webp",
     alt:"Fire extinguishers image ",
     title:"Zedex Fire Company : Your Trusted Partner for Comprehensive Fire Safety Solutions in Delhi NCR and India",
@@ -56,9 +56,9 @@ const blogsCards  = [
 
 {blogsCards.map((items,index)=>(
 
-     <div
+     <Link
                   key={index}
-                 
+                  href={items.href}
                   className=" block bg-white shadow-md rounded overflow-hidden group hover:shadow-lg transition"
                 >
 
@@ -66,7 +66,6 @@ const blogsCards  = [
 
                     <img src={items.img}  alt={items.alt}    className="transition-transform duration-300 group-hover:scale-100 object-cover object-center"
                     />
-                  
 
                     <div className="absolute inset-0 bg-black/30 opacity-0 group-hover:opacity-100 transition">
                       <div className="absolute bottom-3 right-3   bg-red-500 rounded-full" >
@@ -85,14 +84,12 @@ const blogsCards  = [
 <p className="text-md text-justify md:text-left">
 <strong> Published in</strong> in Fire Suppression
 </p>
-<Link  href="/top-fire-fighting-company-in-delhi-ncr">
-  click here
-  </Link>
+
 <p>
  <strong>Tagged under :</strong> Fire fighting system, fire fighting system in Delhi, Fire Safety, fire suppression system
 </p>
                   </div>
-                </div>
+                </Link>
 
 ))}
 
--- src/app/top-Fire-Fighting-Company-in-Delhi-NCR/page.js
+++ src/app/top-Fire-Fighting-Company-in-Delhi-NCR/page.js
@@ -1,21 +1,17 @@
 import TopFireFighting from "./TopFireFighting";
 
- 
 
 export const metadata = {
 
     title: "Top Fire Fighting Company in Delhi NCR | Zedex Fire Services",
-    description: " Looking for the best firefighting company in Delhi NCR? Zedex Fire Services is ISO 9001:2008 certified, offering top-quality systems & equipment. Contact us today!",
+    description: "Looking for the best firefighting company in Delhi NCR? Zedex Fire Services is ISO 9001:2008 certified, offering top-quality systems & equipment. Contact us today!",
   
   }
 
 export default function page() {
     return (
         <>
-
-
       <TopFireFighting/>
-        
         </>
     );
 }
