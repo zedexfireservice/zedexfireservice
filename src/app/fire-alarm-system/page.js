9a3ba3e8086418152c80c8ad9f647b5a8bf6c2e3
Aman Sharma <60314850+amansharma32@users.noreply.github.com>
2025-06-04 17:37:17 +0530

    page zedex 5

--- src/app/fire-alarm-system/page.js
+++ src/app/fire-alarm-system/page.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import FireAlarmSystemBlogPage from './FireAlarmSystemBlogPage'
 
+export const metadata = {
+    title:" Fire Alarm Systems: Installation in Gurugram – Zedex Fire",
+    description:" Protect your Gurugram property with Zedex Fire. Learn what a fire alarm system is, how it works, & why it's vital for safety & compliance. Get a free consultation.",
+}
+
 export default function page() {
   return (
 
