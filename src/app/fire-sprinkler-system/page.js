9f863ba705d79d2b0af382631904247bc13f1887
Aman Sharma <60314850+amansharma32@users.noreply.github.com>
2025-06-24 15:46:57 +0530

    page added 2

--- src/app/fire-sprinkler-system/page.js
+++ src/app/fire-sprinkler-system/page.js
@@ -1,10 +1,69 @@
 import FireSprinklerPage from "./FireSprinklerPage"
 
+
 export const metadata = {
   title: "Fire Sprinkler System in Delhi NCR | ZedEx Fire",
   description: "ZedEx Fire: Expert supplier, distributor & installation of fire sprinkler systems in Delhi NCR. Wide range of types & UL-listed hoses. Get a quote!",
+  keywords: [
+   
+    "Fire sprinkler system",
+    "Fire sprinkler installation Delhi NCR", 
+    "Automatic fire sprinkler",
+    "Wet pipe sprinkler system",
+    "Dry pipe sprinkler system",
+    "Preaction sprinkler system",
+    "Deluge sprinkler system",
+    "Fire suppression system Delhi NCR",
+    "Fire safety system Delhi",
+    "Commercial fire sprinkler",
+    "Industrial fire sprinkler",
+    "Residential fire sprinkler",
+    "Fire sprinkler design",
+    "Fire sprinkler maintenance",
+    "Fire sprinkler supplier Delhi NCR", 
+    "Fire sprinkler distributor Delhi NCR", 
+    "UL-listed hoses", 
+    "NFPA compliant sprinkler system",
+    "Fire sprinkler system types", 
+    "Fire sprinkler system quote", 
+    "Zedex Fire",
+    "Zedex Fire sprinkler",
+    "Zedex Fire safety",
+    "Zedex Fire solutions",
+    "Fire sprinkler system Delhi",
+    "Fire sprinkler system Gurugram",
+    "Fire sprinkler system Noida",
+    "Fire sprinkler system Ghaziabad",
+    "Fire sprinkler system Faridabad",
+    "Fire sprinkler system India",
+  ],
+  robots: "index, follow",
 
-}
+  openGraph: {
+    title: "Fire Sprinkler System in Delhi NCR | ZedEx Fire",
+    description: "ZedEx Fire: Expert supplier, distributor & installation of fire sprinkler systems in Delhi NCR. Wide range of types & UL-listed hoses. Get a quote!",
+    url: "https://www.zedexfire.com/fire-sprinkler-system",
+    type: "product",
+    images: [
+      {
+        url: "https://res.cloudinary.com/dgx5cntyb/image/upload/v1746618656/fire-sprinkler-system-3_w6g04z.webp", 
+        width: 1200,
+        height: 630,
+        alt: "Fire Sprinkler System Installation by ZedEx Fire in Delhi NCR",
+      },
+    ],
+  },
+  alternates: {
+    canonical: "https://www.zedexfire.com/fire-sprinkler-system",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Fire Sprinkler System Delhi NCR - ZedEx Fire",
+    description: "ZedEx Fire: Expert supplier, distributor & installation of fire sprinkler systems in Delhi NCR. Types & UL-listed hoses. Get a quote!",
+    image: "https://res.cloudinary.com/dgx5cntyb/image/upload/v1746618656/fire-sprinkler-system-3_w6g04z.webp", 
+  },
+};
+
 
 export default function page() {
 
