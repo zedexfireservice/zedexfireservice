914d89c1109ef3e58bce54d70e01efe8e5aaa012
Aman Sharma <60314850+amansharma32@users.noreply.github.com>
2025-06-24 16:29:31 +0530

    fire door page seo

--- src/app/fire-door-service/page.js
+++ src/app/fire-door-service/page.js
@@ -1,12 +1,69 @@
 import FireDoorPage from "./FireDoorPage";
 
  
- export const metadata ={
-    title:"Fire Doors in Delhi NCR: Manufacturer, Supplier, and Installation",
-    description:"Zedex Fire : Trusted ISO-certified manufacturer, supplier & installer of quality fire doors in Delhi NCR. Secure your property. Get a free quote!"
- } 
- 
- 
+ export const metadata = {
+  title: "Fire Doors in Delhi NCR: Manufacturer, Supplier, and Installation",
+  description: "Zedex Fire : Trusted ISO-certified manufacturer, supplier & installer of quality fire doors in Delhi NCR. Secure your property. Get a free quote!",
+  keywords: [ 
+    "Fire doors Delhi NCR",
+    "Fire door manufacturer Delhi",
+    "Fire door supplier Delhi",
+    "Fire door installation Delhi",
+    "Fire door service Delhi NCR",
+    "Trusted fire door supplier",  
+    "ISO-certified fire doors",   
+    "Quality fire doors",         
+    "Secure property fire doors",  
+    "Fire doors free quote",       
+    "Fire rated doors",
+    "Commercial fire doors",
+    "Industrial fire doors",
+    "Wooden fire doors",
+    "Steel fire doors",
+    "Fire door solutions",
+    "Fire safety doors",
+    "Fire door repair",
+    "Fire door maintenance",
+    "Compliance fire doors",
+    "Building safety doors", 
+    "Zedex Fire",
+    "Zedex fire doors",
+    "Zedex safety",
+    "Zedex solutions",
+    "ISO 9001:2008 certified", 
+    "Fire doors Delhi",
+    "Fire doors Gurugram",
+    "Fire doors Noida",
+    "Fire doors Ghaziabad",
+    "Fire doors Faridabad",
+    "Fire doors India",
+  ],
+  robots: "index, follow",
+
+  openGraph: {
+    title: "Fire Doors in Delhi NCR: Manufacturer, Supplier, and Installation",
+    description: "Zedex Fire : Trusted ISO-certified manufacturer, supplier & installer of quality fire doors in Delhi NCR. Secure your property. Get a free quote!",
+    url: "https://www.zedexfire.com/fire-door-service",
+    type: "website",  
+    images: [
+      {
+        url: "https://res.cloudinary.com/dgx5cntyb/image/upload/v1746618654/Fire-Door-Service1_rmxcde.webp",  
+        width: 1200,
+        height: 630,
+        alt: "Fire Door Manufacturing, Supply, and Installation by Zedex Fire in Delhi NCR",
+      },
+    ],
+  },
+  alternates: {
+    canonical: "https://www.zedexfire.com/fire-door-service",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Fire Doors Delhi NCR - Manufacturer, Supplier, Installation | Zedex Fire",
+    description: "Zedex Fire: Trusted ISO-certified manufacturer, supplier & installer of quality fire doors in Delhi NCR. Secure your property. Get a free quote!",
+    image: "https://res.cloudinary.com/dgx5cntyb/image/upload/v1746618654/Fire-Door-Service1_rmxcde.webp",  
+  },
+};
 
 export default function page() {
     return (
